dd5733e721f33a0a5dcdf0ceba7a3bc1c407f215
Kate Moser <komoser@gmail.com>
2022-02-11 17:21:56 -0600

    started tests on user model

--- test_user_model.py
+++ test_user_model.py
@@ -40,6 +40,26 @@ class UserModelTestCase(TestCase):
 
         self.client = app.test_client()
 
+        u1 = User(
+            email="test1@test.com",
+            username="testuser1",
+            password="HASHED_PASSWORD1"
+        )
+
+        u2 = User(
+            email="test2@test.com",
+            username="testuser2",
+            password="HASHED_PASSWORD2"
+        )
+
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+
+        self.u1_id = u1.id
+        self.u2_id = u2.id
+        
+
     def test_user_model(self):
         """Does basic model work?"""
 
@@ -55,3 +75,49 @@ class UserModelTestCase(TestCase):
         # User should have no messages & no followers
         self.assertEqual(len(u.messages), 0)
         self.assertEqual(len(u.followers), 0)
+    
+    def test_user_is_following(self):
+        """Test if user1 is following user 2"""
+
+        u1 = User.query.get(self.u1_id)
+        u2 = User.query.get(self.u2_id)
+
+        follow = Follows(
+            user_following_id = u1.id,
+            user_being_followed_id = u2.id
+        )
+
+        db.session.add(follow)
+        db.session.commit()
+
+        # User2 should be in user1's following list
+        self.assertIn(u2, u1.following)
+        # User 1 should be in user 2's followers list
+        self.assertIn(u1, u2.followers)
+
+    def test_user_is_not_following(self):
+        """Test if user1 is following user 2"""
+
+        u1 = User.query.get(self.u1_id)
+        u2 = User.query.get(self.u2_id)
+
+        # User2 should be in user1's following list
+        self.assertNotIn(u2, u1.following)
+        # User 1 should be in user 2's followers list
+        self.assertNotIn(u1, u2.followers)
+
+
+    ##############
+    #Check if METHODS work, not the database!!!!!!
+
+    def test_user_signup(self):
+        """Test if you can make new user"""
+        new_user = User.signup(
+            username = "lupa",
+            email="lupa@dogmail.com",
+            password = "i-love-laura",
+            image_url = None
+        )
+
+        self.assertEqual(new_user.username, "lupa")
+        self.assertIsInstance(new_user, User)
